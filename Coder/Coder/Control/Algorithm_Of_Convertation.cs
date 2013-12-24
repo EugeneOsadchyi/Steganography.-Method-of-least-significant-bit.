19c8c512eb9055913eac075acfeea838fb8a151c
Eugene <silver_od@ukr.net>
2013-12-24 14:38:37 +0200

    Converting message and message's length to BitArray and showing on screen

---
+++ Coder/Coder/Control/Algorithm_Of_Convertation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Collections;
+
+namespace Coder.Control
+{
+    class Algorithm_Of_Convertation
+    {
+        public static void integrateInformation(ref Data_Image_Message obj)
+        {
+            BitArray message = new BitArray(Encoding.ASCII.GetBytes(obj.message));
+            BitArray messageLength = new BitArray(Encoding.ASCII.GetBytes(message.Length.ToString()));
+
+            
+            for (int item = message.Length-1; item>=0; item--)
+            {
+                Console.Write(Convert.ToInt32(message[item]));
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Coder/Coder/Model/Data_Image_Message.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Coder
+{
+    class Data_Image_Message
+    {
+        private Bitmap imageWoInf;
+        private Bitmap imageWiInf;
+
+        public Data_Image_Message(string path, string msg)
+        {
+            image_Without_Info = new Bitmap(path);
+            message = msg;
+        }
+
+        public Bitmap image_Without_Info
+        {
+            get
+            {
+                return imageWoInf;
+            }
+            set
+            {
+                imageWoInf = value;
+            }
+        }
+        
+        public Bitmap image_With_Info
+        {
+            get
+            {
+                return imageWiInf;
+            }
+            set
+            {
+                imageWiInf = value;
+            }
+        }
+
+        public String message { get; set; }
+    }
+}
